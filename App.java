55ca950fb911fcce7d5ab59e8ac910be9ac250df
Abdul_Alsafri_ <abdulalsafri1@gmail.com>
2023-10-04 09:38:22 -0500

    My unfinished calculator code

---
+++ App.java
@@ -0,0 +1,28 @@
+public class App{
+    
+
+    public static void main(String[] args){
+        System.out.println("Welcome, World!");
+
+       static final Scanner inputScanner = new Scanner(System.in);
+       double num1, num2, result;
+       char operator;
+
+       System.out.println("Please enter the 1st number: ");
+       num1 = inputScanner.nextDouble();
+
+       System.out.println("Please enter the 2nd number: ");
+       num2 = inputScanner.nextDouble();
+
+       System.out.println("Choose an operator: ");
+       operator = 
+    }
+
+    private static String getString(String prompt){
+
+    }
+
+    private static double getDouble(String prompt){
+
+    }
+}
